c4bf74b238d89ebdfd71c8ec70b4711bdc36d7fd
Jhonny2345 <147803636+Jhonny2345@users.noreply.github.com>
2023-11-24 12:39:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+alert("Wag ka muna mag fill up, diretso lang click nang log in");
+
+document.getElementById("login-btn").addEventListener("click", function() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+    alert("Please enter username and password");
+    alert("Hi Shane this is your ex boyfriend Jhon Lee");
+    alert("Gusto ko lang sabihin na sobrang miss na miss ko na yung dati");
+    alert("Yung mga panahong interesado kapa sakin, yung mga panahong ang saya saya lang natin");
+    alert("Sobrang miss na miss ko na yun🥺");
+    alert("Sana may pag-asa pa para maibalik yun, sana pwede pa maibalik ulit yung masasayang araw na yun🥺");
+    alert("Sana matutunan moko ulit mahalin🥺");
+    alert("Meron pako gusto sabihin, mag fill up ka sa log in form at click mo log in");
+    
+    
+  } else {
+    alert("Login successful!");
+    alert("Nagpapasalamat ako sayo kasi pinaramdam mo sakin dati yung tunay na saya");
+    alert("Sayo ko lang po naramdaman yun dati, ikaw lang unang nagparamdam sakin ng tunay na saya🥺");
+    alret("Thank you para dun at sana maulit pa yun🥺");
+  }
+});
